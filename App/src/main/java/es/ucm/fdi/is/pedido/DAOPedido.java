0cb9ddb273e4e0ea4497d9856223c141da08dfb8
David Arroyo <david@davidarroyo.es>
2017-05-05 21:33:45 +0200

    DAOs de Pedido

--- App/src/main/java/es/ucm/fdi/is/pedido/DAOPedido.java
+++ App/src/main/java/es/ucm/fdi/is/pedido/DAOPedido.java
@@ -1,5 +1,16 @@
 package es.ucm.fdi.is.pedido;
 
+import java.util.List;
+
+import es.ucm.fdi.is.disco.Disco;
+import es.ucm.fdi.is.usuario.Usuario;
+
 public interface DAOPedido {
 
+	public void crearPedido(Pedido pedido);
+	public void eliminarPedido(Pedido pedido);
+	public List<Pedido> verPedidosUsuario(Usuario usuario);
+	public void addProductoPedido(Disco disco, Pedido pedido);
+	public void actualizarPedido(Pedido antiguo, Pedido nuevo);
+	
 }
--- App/src/main/java/es/ucm/fdi/is/pedido/DAOPedidoImp.java
+++ App/src/main/java/es/ucm/fdi/is/pedido/DAOPedidoImp.java
@@ -1,5 +1,35 @@
 package es.ucm.fdi.is.pedido;
 
+import java.util.List;
+
+import es.ucm.fdi.is.disco.Disco;
+import es.ucm.fdi.is.usuario.Usuario;
+
 public class DAOPedidoImp implements DAOPedido {
+
+	public void crearPedido(Pedido pedido) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void eliminarPedido(Pedido pedido) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public List<Pedido> verPedidosUsuario(Usuario usuario) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public void addProductoPedido(Disco disco, Pedido pedido) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void actualizarPedido(Pedido antiguo, Pedido nuevo) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
