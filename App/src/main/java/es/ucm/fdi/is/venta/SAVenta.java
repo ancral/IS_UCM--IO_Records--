81958216f583ef28e0dfd81ac0f5d69b3c39d0cc
David Arroyo <david@davidarroyo.es>
2017-05-06 17:35:07 +0200

    Completar SA y Transfer de Venta

--- App/src/main/java/es/ucm/fdi/is/venta/SAVenta.java
+++ App/src/main/java/es/ucm/fdi/is/venta/SAVenta.java
@@ -1,5 +1,8 @@
 package es.ucm.fdi.is.venta;
 
 public interface SAVenta {
+	
+	public void generarFactura(Venta venta);
+	public void verBeneficio(Venta venta);
 
 }
--- App/src/main/java/es/ucm/fdi/is/venta/SAVentaImp.java
+++ App/src/main/java/es/ucm/fdi/is/venta/SAVentaImp.java
@@ -2,4 +2,14 @@ package es.ucm.fdi.is.venta;
 
 public class SAVentaImp implements SAVenta {
 
+	public void generarFactura(Venta venta) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void verBeneficio(Venta venta) {
+		// TODO Auto-generated method stub
+		
+	}
+
 }
