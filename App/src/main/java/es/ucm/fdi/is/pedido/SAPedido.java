90d5fb1a9e2aa6ff60dd112c5c496eb88b4ec9ab
ancral <angcruz@ucm.es>
2017-05-17 12:57:21 +0200

    Empezado las SA

--- App/src/main/java/es/ucm/fdi/is/pedido/SAPedido.java
+++ App/src/main/java/es/ucm/fdi/is/pedido/SAPedido.java
@@ -2,17 +2,18 @@ package es.ucm.fdi.is.pedido;
 
 import java.util.List;
 
+import es.ucm.fdi.is.dao.TiendaDatabaseException;
 import es.ucm.fdi.is.disco.Disco;
 import es.ucm.fdi.is.mvc.TiendaObservable;
 import es.ucm.fdi.is.usuario.Usuario;
 
 public interface SAPedido extends TiendaObservable {
 	
-	public void crearPedido(Pedido pedido);
-	public void devolverPedido(Pedido pedido);
-	public List<Pedido> verPedidosUsuario(Usuario usuario);
-	public void addProducto(Pedido pedido, Disco disco);
-	public void modificarPedido(Pedido antiguo, Pedido nuevo);
-	public void eliminar(Pedido pedido);
+	public void crearPedido(Pedido pedido) throws TiendaDatabaseException;
+	public void devolverPedido(Pedido pedido) throws TiendaDatabaseException;
+	public List<Pedido> verPedidosUsuario(Usuario usuario) throws TiendaDatabaseException;
+	public void addProducto(Pedido pedido, Disco disco)  throws TiendaDatabaseException ;
+	public void modificarPedido(Pedido antiguo, Pedido nuevo)  throws TiendaDatabaseException ;
+	public void eliminar(Pedido pedido)  throws TiendaDatabaseException ;
 
 }
